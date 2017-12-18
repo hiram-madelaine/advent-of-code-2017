ff5727fa2d81ef9b6a9d960d78f947c383a6665f
Hiram MADELAINE <hiram.madelaine@gmail.com>
2017-12-18 10:43:03 +0100

    WIP day-14

--- src/advent_of_code/day_14.clj
+++ src/advent_of_code/day_14.clj
@@ -52,8 +52,6 @@
        [(dec x) y]
        [x (inc y)]])))
 
-(adjacents [0 0])
-
 (def x-solution-2
   (comp
     x-day-10
@@ -61,47 +59,81 @@
 
 (defn solution-2
   []
-  (let [size (range 128)
+  (let [width (range 128)
         depth (range 128)
-        matrix (into [] x-solution-2 size)]
-    (into #{}
-          (for [l size
-                r depth
-                :let [row (get matrix l)
-                      b (get row r)]
+        matrix (into [] x-solution-2 width)]
+    (into (sorted-set)
+          (for [w width
+                d depth
+                :let [row (get matrix w)
+                      b (get row d)]
                 :when (= \1 b)]
-            [l r]))))
+            [d w]))))
 
 (defn find-groups
   "Add the notion of already seen."
-  [db group root]
+  [{:keys [db group]} root]
   (let [group (conj group root)
         neighboors (intersection (adjacents root) db)
         not-seen (clojure.set/difference neighboors group)
         group (into group not-seen)]
     (if (seq not-seen)
-      (into  group (mapcat #(find-groups (difference db group) group %) not-seen))
-      group)))
+      (let [{new-db :db adjacent-group :group} (reduce find-groups
+                                                       {:db    (difference db group)
+                                                        :group group}
+                                                       not-seen)]
+        {:group (into group adjacent-group)
+         :db    new-db})
+      {:group group
+       :db    db})))
+
+(defn find-all
+  [result db]
+  (if (seq db)
+    (let [root (first db)
+          {new-db :db group :group} (find-groups {:db    db
+                                                  :group #{}}
+                                                 root)
+          new-db (difference new-db group)]
+      (recur (assoc result root group) new-db))
+    result))
+
 
 (comment
+
+
   (def db (solution-2))
-  (count db)
+  (def result (find-all {} db))
+  (count result)
 
-  )
-
-(comment
-  (difference  #{1 3 5 7} #{7})
-
+  (let [groups (keys result)]
+    (every?  (fn [square]
+               (empty? (filter (adjacents square) result))) groups))
 
 
-  (let [                                                    ;db #{[0 0] [1 0] [0 1] [0 2] [1 2] [2 2] [9 0] [9 1]}
-      result {}
+  (let [db #{[0 0] [1 0] [0 1] [0 2] [1 2] [2 2] [9 0] [9 1]}
+        result {}
         root [0 0]
-        group (find-groups db #{} root)
+        group (find-groups {:db    db
+                            :group #{}}
+                           root)
         result (assoc result root group)]
     result
     (let [new-db (difference db group)
-            new-root (first new-db)
-            new-group (find-groups new-db #{} new-root)]
-        (assoc result new-root new-group)))
+          new-root (first new-db)
+          new-group (find-groups {:db new-db :group #{}} new-root)]
+      (assoc result new-root new-group)))
+
+
+
+
+  (def result )
+
+  (count result)
+
+  (->> result
+       vals
+       (map count)
+       (reduce +))
+
   )
