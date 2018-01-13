3bf1683f2b60f3acdaa7d5ecd02cf0e593aa509c
Hiram MADELAINE <hiram.madelaine@gmail.com>
2018-01-13 11:22:23 +0100

    Bump clojure specter

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  ;[org.clojure/math.combinatorics "0.1.4"]
                  [org.clojure/test.check "0.10.0-alpha2"]
                  [com.gfredericks/test.chuck "0.2.8"]
                  [instaparse "1.4.8"]
-                 [com.rpl/specter "1.0.5"]])
+                 [com.rpl/specter "1.1.0"]])
